1247bc2e87f0ff42c41ea1da6dc6165b5dd79c3b
aminedachir <dachiramine110@gmail.com>
2025-03-10 05:16:24 +0100

    exo4 done

--- exo4.java
+++ exo4.java
@@ -1,21 +1,29 @@
 import java.util.Scanner;
 import java.util.Arrays;
-import java.util.math;
+
 public class exo4{
     public static void main(String[] args){
-        int [10]T;
-        int [10]T_;
+        int[] T={2,3,4,5,6,7,8,9,10,11,12};
+        int[] T_ = new int[11]; 
         int count=0;
-
         for(int i=0;i<70000;i++){
-            int s = (int)math.random(1,6);
-            int z = (int)math.random(1,6);
+            int s = (int) (Math.random() * 6) + 1;
+            int z = (int) (Math.random() * 6) + 1;
             int sum = s+z;
-            for(int i=2;i<13;i++){
-                if (sum == T[i]){
-                    T_ = 
+            for(int j=0;j<10;j++){
+                if(sum == T[j]){
+                    T_[j]++;
                 }
             }
         }
+        int max = T_[0]; 
+        int index = 0; 
+        for (int i = 1; i < 11; i++) { 
+            if (T_[i] > max) {
+                max = T_[i];
+                index = i;
+            }
+        }
+        System.out.println("The most sum is " + T[index] + " it replay " + max);
     }
 }
